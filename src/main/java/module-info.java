91cc4ea330e6295eda9b6fc3773e94793d49b775
jbock <jbock-java@gmx.de>
2022-12-22 09:41:30 +0100

    parmigiano update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module hellofx {
     requires javafx.controls;
     requires javafx.fxml;
-    
+    requires io.parmigiano;
+
     opens uppu to javafx.fxml;
     exports uppu;
 }
--- src/main/java/uppu/S4Checker.java
+++ src/main/java/uppu/S4Checker.java
@@ -1,15 +1,8 @@
 package uppu;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.SplitPane;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import uppu.view.PermutationView;
-
 
 public class S4Checker extends Application {
 
