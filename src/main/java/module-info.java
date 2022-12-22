f1bc012f9700ff49bd17de378056aba253f1faa9
jbock <jbock-java@gmx.de>
2022-12-22 11:45:39 +0100

    add slider

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module hellofx {
     requires javafx.controls;
-    requires javafx.fxml;
     requires io.parmigiano;
 
-    opens uppu to javafx.fxml;
     exports uppu;
 }
--- src/main/java/uppu/view/PermutationView.java
+++ src/main/java/uppu/view/PermutationView.java
@@ -3,11 +3,13 @@ package uppu.view;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.control.SplitPane;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -24,41 +26,42 @@ public class PermutationView {
     public static final Color GRAY = Color.rgb(64, 64, 64);
 
     private final Canvas canvas = new Canvas(WIDTH_CANVAS, HEIGHT);
-    
-    private final Stage stage;
-    private final Scene scene;
-    private final SplitPane splitPane;
 
-    private PermutationView(Stage stage, Scene scene, SplitPane splitPane) {
+    private final Stage stage;
+    private final SplitPane splitPane = new SplitPane();
+    private final BorderPane borderPane = new BorderPane();
+    private final Slider slider = new Slider(0, 32, INITIAL_SPEED);
+
+
+    private PermutationView(Stage stage) {
         this.stage = stage;
-        this.scene = scene;
-        this.splitPane = splitPane;
     }
 
     public static PermutationView create(Stage stage) {
-        SplitPane sp = new SplitPane();
-        Scene scene = new Scene(sp);
-        scene.setFill(Color.BLACK);
+        return new PermutationView(stage);
+    }
+
+    public void init() {
+        createElements();
+        borderPane.setCenter(splitPane);
+        borderPane.setBottom(slider);
+        Scene scene = new Scene(borderPane);
         scene.addEventHandler(KeyEvent.KEY_PRESSED, t -> {
             if (t.getCode() == KeyCode.ESCAPE) {
                 stage.close();
             }
         });
-        return new PermutationView(stage, scene, sp);
-    }
-
-    public void init() {
-        stage.setWidth(WIDTH_CANVAS + WIDTH_PANEL);
-        stage.setHeight(HEIGHT + HEIGHT_SLIDER);
-        createElements();
         stage.setScene(scene);
     }
 
     private void createElements() {
         StackPane sp1 = new StackPane();
+        sp1.setMaxWidth(WIDTH_CANVAS);
+        sp1.setMinWidth(WIDTH_CANVAS);
         sp1.getChildren().add(canvas);
         sp1.setBackground(new Background(new BackgroundFill(GRAY, null, null)));
         StackPane sp2 = new StackPane();
+        sp2.setMinWidth(WIDTH_PANEL);
         sp2.setBackground(new Background(new BackgroundFill(GRAY, null, null)));
         sp2.getChildren().add(new Button("OK"));
         splitPane.getItems().addAll(sp1, sp2);
