c9b16817ef252932bba1736f0ea9fedff3c204b9
jbock <jbock-java@gmx.de>
2022-12-26 12:24:03 +0100

    test the rotation

--- src/main/java/uppu/model/Rotation.java
+++ src/main/java/uppu/model/Rotation.java
@@ -2,12 +2,64 @@ package uppu.model;
 
 import javafx.geometry.Point3D;
 
-public record Rotation(Point3D row0, Point3D row1, Point3D row2) {
+import java.util.function.DoubleUnaryOperator;
 
-    public static Rotation fromAxis(Point3D u, double angle) {
-        double ux = u.getX();
-        double uy = u.getY();
-        double uz = u.getZ();
-        return null;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
+public record Rotation(
+        DoubleUnaryOperator r00,
+        DoubleUnaryOperator r10,
+        DoubleUnaryOperator r20,
+        DoubleUnaryOperator r01,
+        DoubleUnaryOperator r11,
+        DoubleUnaryOperator r21,
+        DoubleUnaryOperator r02,
+        DoubleUnaryOperator r12,
+        DoubleUnaryOperator r22) {
+
+    public static Rotation fromAxis(Point3D u) {
+        u = u.normalize();
+        double x = u.getX();
+        double y = u.getY();
+        double z = u.getZ();
+        double xx = x * x;
+        double yy = y * y;
+        double zz = z * z;
+        double xy = x * y;
+        double xz = x * z;
+        double yz = y * z;
+        return new Rotation(
+                // ROW_0
+                t -> {
+                    double cos_t = cos(t);
+                    return cos_t + xx * (1 - cos_t);
+                },
+                t -> xy * (1 - cos(t)) - z * sin(t),
+                t -> xz * (1 - cos(t)) + y * sin(t),
+                // ROW_1
+                t -> xy * (1 - cos(t)) + z * sin(t),
+                t -> {
+                    double cos_t = cos(t);
+                    return cos_t + yy * (1 - cos_t);
+                },
+                t -> yz * (1 - cos(t)) - x * sin(t),
+                // ROW_2
+                t -> xz * (1 - cos(t)) - y * sin(t),
+                t -> yz * (1 - cos(t)) + x * sin(t),
+                t -> {
+                    double cos_t = cos(t);
+                    return cos_t + zz * (1 - cos_t);
+                });
+    }
+
+    public Point3D apply(Point3D p, double t) {
+        double x = p.getX();
+        double y = p.getY();
+        double z = p.getZ();
+        double x2 = r00.applyAsDouble(t) * x + r01.applyAsDouble(t) * y + r02.applyAsDouble(t) * z;
+        double y2 = r10.applyAsDouble(t) * x + r11.applyAsDouble(t) * y + r12.applyAsDouble(t) * z;
+        double z2 = r20.applyAsDouble(t) * x + r21.applyAsDouble(t) * y + r22.applyAsDouble(t) * z;
+        return new Point3D(x2, y2, z2);
     }
 }
--- src/test/java/uppu/model/RotationTest.java
+++ src/test/java/uppu/model/RotationTest.java
@@ -3,12 +3,27 @@ package uppu.model;
 import javafx.geometry.Point3D;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class RotationTest {
+
+    @Test
+    void testZero() {
+        Point3D p = new Point3D(1, 2, 3);
+        Rotation rotation = Rotation.fromAxis(new Point3D(4, 2, 5));
+        Point3D result = rotation.apply(p, 0);
+        assertTrue(p.distance(result) < 0.000001);
+    }
 
     @Test
     void testFromAxis() {
         Point3D p0 = HomePoints.homePoints().get(0);
         Point3D p1 = HomePoints.homePoints().get(1);
-        System.out.println(p0);
+        Point3D p2 = HomePoints.homePoints().get(2);
+        Point3D center = p0.add(p1).add(p2).multiply(1f / 3f);
+        Point3D v = p0.subtract(center);
+        Rotation rotation = Rotation.fromAxis(center);
+        Point3D result = rotation.apply(v, -Math.PI * 2f / 3f);
+        assertTrue(p1.distance(center.add(result)) < 0.000001);
     }
 }
